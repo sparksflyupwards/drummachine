656c801a0d0df9d0390aa257bb02fcbb1479a172
sparksflyupwards <saad.jamil.khan@gmail.com>
2021-03-08 11:53:56 -0500

    removed old drum-machine folder

--- drum-machine/src/components/AppTitle/AppTitle.js
+++
@@ -1,7 +0,0 @@
-import "./AppTitle.css";
-
-const AppTitle = () => {
-  return <div id="title">DRUM PAD</div>;
-};
-
-export default AppTitle;
--- drum-machine/src/components/DrumPad/DrumPad.stories.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import DrumPad from './DrumPad';
-
-storiesOf('DrumPad', module).add('default', () => <DrumPad />);
